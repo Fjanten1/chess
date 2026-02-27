45d5f45ff791d45e79b50154ca97b6399b597836
adpe2400 <adpe2400@student.miun.se>
2026-02-27 17:12:00 +0100

    fix: Board initialization and piece color assignment

--- board.py
+++ board.py
@@ -12,39 +12,39 @@ class Board:
 
     def setup_board(self):
         """Add all the pieces to the board."""
-        # Black pieces
-        self.squares['a1'] = Rook('BLACK', 1)
-        self.squares['b1'] = Knight('BLACK', 1)
-        self.squares['c1'] = Bishop('BLACK', 1)
-        self.squares['d1'] = Queen('BLACK', 1)
-        self.squares['e1'] = King('BLACK', 1)
-        self.squares['f1'] = Bishop('BLACK', 2)
-        self.squares['g1'] = Knight('BLACK', 2)
-        self.squares['h1'] = Rook('BLACK', 2)
+        # White pieces (bottom row)
+        self.squares['a1'] = Rook('WHITE', 1)
+        self.squares['b1'] = Knight('WHITE', 1)
+        self.squares['c1'] = Bishop('WHITE', 1)
+        self.squares['d1'] = Queen('WHITE', 1)
+        self.squares['e1'] = King('WHITE', 1)
+        self.squares['f1'] = Bishop('WHITE', 2)
+        self.squares['g1'] = Knight('WHITE', 2)
+        self.squares['h1'] = Rook('WHITE', 2)
 
-        # Black pawns
-        black_pawns = {
-            f"{chr(col)}2": Pawn('BLACK', col - ord('a') + 1)
-            for col in range(ord('a'), ord('i'))
-        }
-        self.squares.update(black_pawns)
-
-        # White pieces
-        self.squares['a8'] = Rook('WHITE', 1)
-        self.squares['b8'] = Knight('WHITE', 1)
-        self.squares['c8'] = Bishop('WHITE', 1)
-        self.squares['d8'] = Queen('WHITE', 1)
-        self.squares['e8'] = King('WHITE', 1)
-        self.squares['f8'] = Bishop('WHITE', 2)
-        self.squares['g8'] = Knight('WHITE', 2)
-        self.squares['h8'] = Rook('WHITE', 2)
-
-        # White pawns
+        # White pawns (second row)
         white_pawns = {
-            f"{chr(col)}7": Pawn('WHITE', col - ord('a') + 1)
+            f"{chr(col)}2": Pawn('WHITE', col - ord('a') + 1)
             for col in range(ord('a'), ord('i'))
         }
         self.squares.update(white_pawns)
+
+        # Black pieces (top row)
+        self.squares['a8'] = Rook('BLACK', 1)
+        self.squares['b8'] = Knight('BLACK', 1)
+        self.squares['c8'] = Bishop('BLACK', 1)
+        self.squares['d8'] = Queen('BLACK', 1)
+        self.squares['e8'] = King('BLACK', 1)
+        self.squares['f8'] = Bishop('BLACK', 2)
+        self.squares['g8'] = Knight('BLACK', 2)
+        self.squares['h8'] = Rook('BLACK', 2)
+
+        # Black pawns (second from top row)
+        black_pawns = {
+            f"{chr(col)}7": Pawn('BLACK', col - ord('a') + 1)
+            for col in range(ord('a'), ord('i'))
+        }
+        self.squares.update(black_pawns)
 
         # Set initial positions and boards
         for square, piece in self.squares.items():
