eed35e5a581012d0a622a85db3de00a6ae122754
adpe2400 <adpe2400@student.miun.se>
2026-02-27 17:20:17 +0100

    feat: Test

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 # chess/main.py
-from pieces import Pawn
+from board import Board
 
-pawn = Pawn('BLACK', 1)
+# Create a board
+board = Board()
+
+# Test moving a pawn
+pawn = board.get_piece('a2')
 pawn.move()
+
+# Test moving a rook
+rook = board.get_piece('a1')
+rook.move('forward', 1)
+
+# Test moving a bishop
+bishop = board.get_piece('c1')
+bishop.move('forward_right', 2)
+
+# Test moving a knight
+knight = board.get_piece('b1')
+knight.move('forward_left')
+
+# Test moving a king
+king = board.get_piece('e1')
+king.move('forward')
+
+# Test moving a queen
+queen = board.get_piece('d1')
+queen.move('forward_right', 3)
